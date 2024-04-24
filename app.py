c342fd0449b0e1086f32f00f299668531f14113a
norhan karm <142645218+Nourhankarm@users.noreply.github.com>
2024-04-24 21:41:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def get_detections():
         abort(404)
 
 
-if _name_ == '_main_':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
