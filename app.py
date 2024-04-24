af77dbdcf4a2ffa972521de23a332da83f351220
norhan karm <142645218+Nourhankarm@users.noreply.github.com>
2024-04-24 22:35:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,66 @@
-from flask import Flask, request, jsonify
+from absl import app, logging
+import numpy as np
 import pickle
 
+from flask import Flask, request, jsonify, abort
+import os
+
+# Initialize Flask application
 app = Flask(__name__)
 
-# Load your trained model (make sure the path is accessible from your Jupyter Notebook)
-#model = pickle.load(open('rf.pkl', 'rb'))
 # Load the model from a pickle file
 model_file_path = 'diabetes_model.sav'
 with open(model_file_path, 'rb') as file:
     model = pickle.load(file)
 
-@app.route('/')
-def home():
-    return "Welcome to the Diabetes Prediction API"
+# Correct the syntax error in the path and load the encoder
+encoder_file_path = 'encoder.sav'  # Corrected path
+with open(encoder_file_path, 'rb') as file:
+    encoder = pickle.load(file)
 
+# API endpoint for predictions
 @app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    print(data["gender"])
-    features = [data['gender'], data['age'], data['hypertension'], data['heart_disease'],
-                data['smoking_history'], data['bmi'], data['HbA1c_level'], data['blood_glucose_level']]
-    prediction = model.predict([features])
-    return jsonify({'diabetes_prediction': int(prediction[0])})
+def get_detections():
+    try:
+        data = request.json
+        print(data)
+        gender = data['gender']
+        age = data['age']
+        hypertension = data['hypertension']
+        heart_disease =data['heart_disease']
+        smoking_history = data['smoking_history']
+        bmi = data['bmi']
+        HbA1c_level = data['HbA1c_level']
+        blood_glucose_level = data['blood_glucose_level'] 
+        print(f"data received: gender= {data['gender']}, age= {age}, hypertension= {hypertension}, heart_disease= {heart_disease}, smoking_history= {data['smoking_history']}, bmi= {bmi}, HbA1c_level= {HbA1c_level}, blood_glucose_level= {blood_glucose_level}")
+        #  'gender' and 'smoking_history' are categorical and need to be encoded
+        print(encoder.classes_)
+        encoded_smoking_history = encoder.transform([smoking_history])[0]
+        print(encoded_smoking_history)
+        if(gender=="Male"):
+            encoded_gender=1
+        elif (gender=="Female"):
+            encoded_gender=0
+        else:
+            encoded_gender=2
+        
+        
+
+        # Log received data
+        print(f"data received: gender= {data['gender']}, age= {age}, hypertension= {hypertension}, heart_disease={heart_disease}, smoking_history= {data['smoking_history']}, bmi= {bmi}, HbA1c_level= {HbA1c_level}, blood_glucose_level= {blood_glucose_level}")
+
+        # Prepare the model input by replacing 'gender' and 'smoking_history' with their encoded forms
+        model_input = np.array([[encoded_gender, age, hypertension, heart_disease, encoded_smoking_history, bmi, HbA1c_level, blood_glucose_level]])
+
+        # Make prediction
+        prediction = model.predict(model_input)
+
+        # Log and return the prediction
+        print(f"prediction: {prediction}")
+        return jsonify({"prediction": int(prediction[0])})
+
+    except FileNotFoundError:
+        abort(404)
 
 
 if __name__ == '__main__':
