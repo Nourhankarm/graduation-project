7af0fdcc12932ede594ecb72131fe9da0ccdb77e
norhan karm <142645218+Nourhankarm@users.noreply.github.com>
2024-04-25 02:03:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pickle
 app = Flask(__name__)
 
 # Load your trained model (make sure the path is accessible from your Jupyter Notebook)
-#model = pickle.load(open('model.sav', 'rb'))
+#model = pickle.load(open('decision_tree_model.sav', 'rb'))
 # Load the model from a pickle file
-model_file_path = 'model.sav'
+model_file_path = 'decision_tree_model.sav'
 with open(model_file_path, 'rb') as file:
     model = pickle.load(file)
 
