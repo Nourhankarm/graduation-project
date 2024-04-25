1bf2d9ed8d74ecc70e2d6cea2a378b9990e5af15
norhan karm <142645218+Nourhankarm@users.noreply.github.com>
2024-04-25 05:29:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def get_detections():
         abort(500, description="Prediction failed")
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)werkzeug.exceptions.InternalServerError: 500
+    app.run(debug=False, host='0.0.0.0', port=5000)
