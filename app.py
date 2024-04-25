a6e3647dfee90ba7c6ff776ad21a8421bc87b60b
norhan karm <142645218+Nourhankarm@users.noreply.github.com>
2024-04-25 05:37:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def get_detections():
         abort(500, description="Prediction failed")
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
